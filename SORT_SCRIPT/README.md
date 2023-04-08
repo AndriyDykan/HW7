bc76258b00623d57847864bb4e13d7eaae8c70ee
AndriyDykan <dykanandriy@gmail.com>
2023-04-08 20:25:11 +0300

    SCRIPT

---
+++ SORT_SCRIPT/README.md
@@ -0,0 +1,8 @@
+MY SORT SCRIPT
+___________________________________________
+Script can:
+-Delete hollow dir in path you write in cmd
+-Unpack archives
+-Sort all known file by their type
+____________________________________________
+If you want to change amount of dir or file type , you can just change key and values in dictionary (dict_of_known_fileformat)
---
+++ SORT_SCRIPT/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+
+setuptools.setup(
+    name='SORT_SCRIPT',
+    version='0.0.1',
+    packages=setuptools.find_namespace_packages(),
+    url='',
+    license='"MIT"',
+    author='MIRAN',
+    author_email='andriy.dykanan@gmail.com',
+    description='sort script for dir',
+    entry_points={'console_scripts':['sort = src.main:main']}
+)
